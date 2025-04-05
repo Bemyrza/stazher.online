be25c4f67023909bbe3c2e637959b16b9ac84016
bekmyrza <mukamidinovbekmyrza@gmail.com>
2025-04-05 22:01:17 +0600

    lord

---
+++ src/views/Partners.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import ChooseTalanter from "@/components/pages/Pathers/ChooseTalanter.vue"
+import CompanyUS from "@/components/pages/Pathers/CompanyUS.vue";
+</script>
+
+
+<template>
+    <ChooseTalanter />
+    <CompanyUS />
+
+</template>
+
+
+<style scoped></style>
